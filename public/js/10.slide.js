dbf493cf6ebbfbd9bcc4060cd04f5c72a759d48a
lol0320 <lol0320@naver.com>
2020-09-15 11:54:30 +0900

    변수에 넣어서 사용

--- public/js/10.slide.js
+++ public/js/10.slide.js
@@ -31,3 +31,45 @@ $(".wrapper1 .slide-wrap").stop().animate({"left":-n*100+"%"}, 500);
 	setInterval(ani, 3000);
 })();
 
+
+
+// 시험장 perv, next
+(function(){
+var $slideWrap =$(".wrapper3 .slide-wrap");
+var $pagerSpan =$(".wrapper3 .pager-wrap span");
+var $btnPrev=$("wrapper3 .btn-prev")
+var $btnNext=$("wrapper3 .btn-next")
+	
+var n = 0;
+	
+	$btnPrev.click(onPrev);
+	$btnNext.click(onNext);
+	
+	function onPrev(){
+	if(n>0) n--;
+	
+	ani();
+	}
+	function onNext(){
+if(n<3)n++;
+		ani();
+	}
+	function ani(){
+$slideWrap.stop().animate({"left": -n*100+"%"}, 500, pegerToggle);
+
+	}
+	function pegerToggle(){
+		$pagerSpan.show();
+
+if(n == 0){
+	$pagerSpan.eq(0).hide();
+	$pagerSpan.eq(1).hide();
+}
+if(n == 3){
+	$pagerSpan.eq(1).hide();
+	$pagerSpan.eq(2).hide();
+}
+	}
+	pegerToggle();
+})();
+
